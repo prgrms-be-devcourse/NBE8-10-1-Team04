2d17aacd5f3ec2c9a746788a1e4f9999ce4dae8f
MintyU <mintyu.dev@gmail.com>
2025-12-15 16:37:30 +0900

    [feat] POST product api 구현

--- Backend/src/main/java/com/back/domain/product/entity/Product.java
+++ Backend/src/main/java/com/back/domain/product/entity/Product.java
@@ -2,26 +2,34 @@ package com.back.domain.product.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Builder
 public class Product extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @Column(nullable = false, length = 50)
     private String name;
 
     @Column(nullable = false)
-    private Integer price;
+    private int price;
 
     @Column(length = 200)
     private String description;
 
-    public Product(String name, int price) {
+    public Product(String name, int price, String description) {
         this.name = name;
         this.price = price;
+        this.description = description;
+    }
+
+    public static Product create(String name, Integer price, String description) {
+        return Product.builder()
+                .name(name)
+                .price(price)
+                .description(description)
+
+                .build();
     }
 }
