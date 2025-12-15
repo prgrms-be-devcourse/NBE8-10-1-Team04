a5b734586765d8bd913b43e036f4ff75420cbff8
MintyU <mintyu.dev@gmail.com>
2025-12-15 15:35:22 +0900

    [feat] Product 엔티티 구조 변경

--- Backend/src/main/java/com/back/domain/product/entity/Product.java
+++ Backend/src/main/java/com/back/domain/product/entity/Product.java
@@ -1,14 +1,24 @@
 package com.back.domain.product.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
 @Getter
 public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
     private String name;
-    private int price;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(length = 200)
+    private String description;
 
     public Product(String name, int price) {
         this.name = name;
