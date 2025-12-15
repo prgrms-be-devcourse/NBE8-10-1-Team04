f0581b1850952dd4d9e6b86213f5b3d78f5348e2
BaekRaven <yjw70809@gmail.com>
2025-12-15 14:30:51 +0900

    [feat] product entity,repository 구현

--- Backend/src/main/java/com/back/domain/product/entity/Product.java
+++ Backend/src/main/java/com/back/domain/product/entity/Product.java
@@ -1,4 +1,17 @@
 package com.back.domain.product.entity;
 
-public class product {
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Product extends BaseEntity {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
--- Backend/src/main/java/com/back/domain/product/repository/ProductRepository.java
+++ Backend/src/main/java/com/back/domain/product/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.back.domain.product.repository;
 
-public class ProductRepository {
+import com.back.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
