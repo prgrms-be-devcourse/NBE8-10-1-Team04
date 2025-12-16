e075baba72da18b3dfc74c309526617e70e2d3f0
MintyU <mintyu.dev@gmail.com>
2025-12-16 10:26:51 +0900

    [feat] product DTO 통합

--- Backend/src/main/java/com/back/global/initData/BaseInitData.java
+++ Backend/src/main/java/com/back/global/initData/BaseInitData.java
@@ -10,22 +10,12 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 
-import com.back.domain.product.dto.ProductCreateReq;
-import com.back.domain.product.entity.Product;
 import com.back.domain.product.service.ProductService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -48,10 +38,8 @@ public class BaseInitData {
     @Transactional
     public void work1() {
         if (productService.count() > 0) return;
-        ProductCreateReq productCreateReq1 = new ProductCreateReq("갤럭시", 1000, "삼성껍니다.");
-        productService.create(productCreateReq1);
-        ProductCreateReq productCreateReq2 = new ProductCreateReq("아이폰", 5000, "애플껍니다.");
-        productService.create(productCreateReq2);
+        productService.create("갤럭시", 1000, "삼성껍니다.");
+        productService.create("아이폰", 5000, "애플껍니다.");
     }
     @PostConstruct
     @Transactional
