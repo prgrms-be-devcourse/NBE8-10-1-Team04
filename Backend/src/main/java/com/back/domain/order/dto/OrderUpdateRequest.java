3a7b99c1840288a290061f422088fdf1b689b521
kimhss <hyunuary108@kyonggi.ac.kr>
2025-12-17 23:24:11 +0900

    [feat] 백엔드 주문 전송/수정 유효성 추가#88

--- Backend/src/main/java/com/back/domain/order/dto/OrderUpdateRequest.java
+++ Backend/src/main/java/com/back/domain/order/dto/OrderUpdateRequest.java
@@ -1,10 +1,19 @@
 package com.back.domain.order.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public class OrderUpdateRequest {
+    @NotBlank
     private String address;
+    @NotBlank
     private String zipCode;
+    @NotNull
+    @Size(min = 1)
     private List<OrderProductDto> products;
 
     public String getAddress() {return address;}
