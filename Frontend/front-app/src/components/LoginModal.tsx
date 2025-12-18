e81e5664ed52791ec5e06ddc94e3cc9339e15108
MintyU <mintyu.dev@gmail.com>
2025-12-18 16:04:23 +0900

    [refactor] Security 관련 UI 개선

--- Frontend/front-app/src/components/LoginModal.tsx
+++ Frontend/front-app/src/components/LoginModal.tsx
@@ -35,21 +35,22 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-xl w-80">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <div className="bg-white p-6 rounded-lg shadow-xl w-100 flex flex-col gap-2">
         <h2 className="text-xl font-bold mb-4">관리자 로그인</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}
+          className="flex flex-col gap-2">
           <input
             type="text"
             placeholder="ID"
-            className="w-full border p-2 mb-2"
+            className="w-full border p-2 mb-2 rounded-md border-gray-300"
             value={adminName}
             onChange={(e) => setAdminName(e.target.value)}
           />
           <input
             type="password"
             placeholder="PW"
-            className="w-full border p-2 mb-4"
+            className="w-full border p-2 mb-4 rounded-md border-gray-300"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
@@ -57,14 +58,14 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
           <div className="flex gap-2">
             <button
               type="submit"
-              className="flex-1 bg-blue-500 text-white py-2 rounded"
+              className="flex-1 bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
             >
               로그인
             </button>
             <button
               type="button"
               onClick={onClose}
-              className="flex-1 bg-gray-200 py-2 rounded"
+              className="flex-1 bg-gray-200 py-2 rounded hover:bg-gray-300"
             >
               취소
             </button>
